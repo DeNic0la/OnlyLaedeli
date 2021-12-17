b2c3239183866b55cff0d59dda2b77fcfb7f85c8
DeNic0la <nicola@fioretti.info>
2021-12-18 00:36:52 +0100

    Finished Dockerizeation

--- backend/backend.js
+++ backend/backend.js
@@ -4,8 +4,8 @@ import {ApiRouter} from "./api/ApiRouter.js";
 
 const app = new Application();
 
-
 app.use(ApiRouter.routes());
 app.use(ApiRouter.allowedMethods());
 app.use(staticFiles);//This needs to be last (lowest Prio)
+console.log("Started");
 app.listen({port:8000});
