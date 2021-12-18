60c52f131b4cfafae1cf45754efd4b3e96b42492
DeNic0la <nicola@fioretti.info>
2021-12-18 22:39:06 +0100

    Added list item

---
+++ frontend/src/components/header/HeaderBasketMenuListItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-list-item>
+    <v-list-item-avatar
+        :tile="true"
+    >
+      <v-img :src="item.avatar"></v-img>
+    </v-list-item-avatar>
+
+    <v-list-item-content>
+      <v-list-item-title>{{item.title}}</v-list-item-title>
+      <v-list-item-subtitle>{{item.subtitle}}</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: "HeaderBasketMenuListItem",
+  props:{
+    id:{
+      type:Number,
+      required:true
+    },
+    image:{
+      type:String,
+      required:false
+    },
+    name:{
+      type:String,
+      required:true
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
