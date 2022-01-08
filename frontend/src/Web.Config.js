9c8414263a00d59167dc90140b7d8c1932746b57
DeNic0la <nicola@fioretti.info>
2022-01-08 15:45:11 +0100

    Commmit for Deployment on Sloppy

--- frontend/src/Web.Config.js
+++ frontend/src/Web.Config.js
@@ -1,5 +1,5 @@
-const baseUrl = 'http://localhost';
-const backendPort = ':8000';
+const baseUrl = 'https://laedeli-back.sloppy.zone';
+const backendPort = '';
 const frontendPort = '';
 const URL_FRONT = `${baseUrl}${frontendPort}`;
 const URL_BACK = `${baseUrl}${backendPort}`;
