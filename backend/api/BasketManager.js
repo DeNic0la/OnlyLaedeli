0691e58fab1faa752c14cf2d309bb3cad7963fd9
laurin-dev <lauloetscher@gmail.com>
2022-01-12 11:11:01 +0100

    add BasketManager

---
+++ backend/api/BasketManager.js
@@ -0,0 +1,20 @@
+export class BasketManager{
+
+    constructor() {
+        this.basketlist = [];
+
+    }
+    addOrUpdateBasket(context, basket){
+
+        context.state.session.set("basket",basket);
+    }
+    async getBasket(context) {
+        if (await context.state.session.has("basket")){
+            return await context.state.session.get("basket");
+        } else {
+            return {};
+        }
+    }
+
+
+}
