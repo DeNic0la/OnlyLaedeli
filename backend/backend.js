f87fce735b6e3cc0d103c5d866bc993e320f3f9e
DeNic0la <nicola@fioretti.info>
2022-01-13 18:38:59 +0100

    tried to fix cors (not working yet...)

--- backend/backend.js
+++ backend/backend.js
@@ -3,12 +3,16 @@ import {staticFiles} from "./fileserver.js";
 import {ApiRouter, SessionForApp} from "./api/ApiRouter.js";
 
 const app = new Application();
+app.use(oakCors({
+    origin: "http://localhost:8080",
+    methods:['GET','POST','OPTION','HEAD'],
+    preflightContinue:false,
+    exposedHeader:"Access-Control-Allow-Origin",
+    optionsSuccessStatus:200
+})); //Enable CORS (Everywhere)
 
 app.use(SessionForApp);// To Use Sessions
-app.use(oakCors()); //Enable CORS (Everywhere)
-ApiRouter.options("", oakCors())
 app.use(ApiRouter.routes());
-app.use(ApiRouter.allowedMethods());
 app.use(staticFiles);//This needs to be last (lowest Prio)
 console.log("Started");
 app.listen({port:8000});
